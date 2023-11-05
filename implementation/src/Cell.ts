8c84acc7a9b83c420a77ae9363a9f73aa9d1de81
ciorobeabianca <110411594+ciorobeabianca@users.noreply.github.com>
2023-11-05 17:27:00 -0500

    Added clear function and other cell functions

--- implementation/src/Cell.ts
+++ implementation/src/Cell.ts
@@ -1,54 +1,49 @@
 import { CellContent } from "./model/Interfaces/ICellContent";
 
 /**
-* Cell represents a cell from the spreadsheet. Each Cell object contains a cell content,
-* a row number and a column number.
-*/
+ * Cell represents a cell from the spreadsheet. Each Cell object contains a cell content,
+ * a row number and a column number.
+ */
 export class Cell {
-    private content: CellContent;  // the content of the cell
-    private row: number;  // the row number of the cell
-    private col: number;  // the column number of the cell
- 
- 
-    /**
-     * Constructor to initialize the cell with the content, row number and column number.
-     */
-    constructor(content: CellContent, row: number, col: number) {
-        this.content = content;
-        this.row = row;
-        this.col = col;
-    }
- 
- 
-    /**
-     * Method to clear or reset the cell content.
-     */
-    public clear(): void {
-        // …
-    }
- 
- 
-    /**
-    * Method to set the value of the cell content.
-    */
-    public setCellContent(value: CellContent): void {
-        this.content = value;
-    }
- 
- 
-    /**
-     * Method to get the value of the cell content.
-     */
-    public getCellContent(): CellContent {
-        return this.content;
-    }
- 
+  private content: CellContent | null; // the content of the cell
+  private row: number; // the row number of the cell
+  private col: number; // the column number of the cell
+
+  /**
+   * Constructor to initialize the cell with the content, row number and column number.
+   */
+  constructor(content: CellContent | null, row: number, col: number) {
+    this.content = content;
+    this.row = row;
+    this.col = col;
+  }
+
+  /**
+   * Method to clear or reset the cell content.
+   */
+  public clear(): void {
+    this.content = null;
+  }
+
+  /**
+   * Method to set the value of the cell content.
+   */
+  public setCellContent(value: CellContent): void {
+    this.content = value;
+  }
+
+  /**
+   * Method to get the value of the cell content.
+   */
+  public getCellContent(): CellContent | null {
+    return this.content;
+  }
  
     /**
      * Method to set the column number to a given value.
      */
     public setCol(colNr: number): void {
-        // …
+        this.col = colNr;
     }
  
  
@@ -64,7 +59,7 @@ export class Cell {
      * Method to set the row number to a given value.
      */
     public setRow(rowNr: number): void {
-        // …
+        this.row = rowNr;
     }
  
  
